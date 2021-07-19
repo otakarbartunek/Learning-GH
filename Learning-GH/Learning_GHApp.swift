0f30dc3374cc4f5a2ef16e507920097e2297e365
Ota <bartunek.ota@gmail.com>
2021-07-19 20:45:19 +0200

    Initial Commit

---
+++ Learning-GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning-GH
+//
+//  Created by Otakar Bartůněk on 19.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
